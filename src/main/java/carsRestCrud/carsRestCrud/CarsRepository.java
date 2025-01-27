f757b859a1972f8242b7ca56c38ed5aed0d0d4da
biggiebroo <sarvar.kulmatov.bigbro@gmail.com>
2025-01-27 12:47:27 +0100

    Deleted not necessary parts

--- src/main/java/carsRestCrud/carsRestCrud/CarsRepository.java
+++
@@ -1,8 +0,0 @@
-package carsRestCrud.carsRestCrud.Cars;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CarsRepository extends JpaRepository<Cars, Long> {
-}
